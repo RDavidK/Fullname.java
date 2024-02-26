21bbdf629cdfdb55011f411e4475030075f7401d
RDavidK <160715787+RDavidK@users.noreply.github.com>
2024-02-26 10:36:29 +0200

    Create Fullname.java

---
+++ Fullname.java
@@ -0,0 +1,7 @@
+class Fullname {
+  public static void main(String[] args) {
+    System.out.println( "David Kinghorn" )
+    System.out.println( "ST10439514@vcconnect.edu.za" )
+    System.out.println( "0606996553" )
+  }
+}
